2191a2fbefd8258e257b1ffff55f214b9ac09ab4
wooilkim <wooilkim@korea.ac.kr>
2021-03-15 23:32:48 +0900

    in progress

--- src/kakao/2018/lv2/compression.py
+++ src/kakao/2018/lv2/compression.py
@@ -1,1 +1,16 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17684
+
+def solution(msg):
+    answer = []
+    dic = [chr(i) for i in range(ord('A'), ord('Z') + 1)]
+    i, j = 0, 0
+    while (i < len(msg)):
+        if msg[i:j] in dic:
+            print(i, j, msg[i:j])
+
+    return answer
+
+
+if __name__ == '__main__':
+    msg = input()
+    print(solution(msg))
