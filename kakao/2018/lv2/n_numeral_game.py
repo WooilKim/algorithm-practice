198f1ef9476a6973183bee0955ad77710c2eea64
wooilkim <wooilkim@korea.ac.kr>
2021-03-17 13:47:27 +0900

    error fix in progress

--- kakao/2018/lv2/n_numeral_game.py
+++ kakao/2018/lv2/n_numeral_game.py
@@ -15,33 +15,46 @@ def convert(num, base):
     return ''.join(q)
 
 
+# 진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
 def solution(n: int, t: int, m: int, p: int):
-    # 진법 n, 미리 구할 숫자의 갯수 t, 게임에 참가하는 인원 m, 튜브의 순서 p
     answer = list()
     x = 0
     base = 0
-    order = 0
+    order = 1
     while x < t:
+        # print('x', x)
         total_turn = m * x + p - 1
         if total_turn == 0:
             answer.append('0')
             x += 1
-        next = order * (n ** order - n ** (order - 1))
+            continue
+
+        next = int(order * (n ** order - n ** (order - 1)))
+
         while total_turn >= base + next:
             base += next
             order += 1
             next = order * (n ** order - n ** (order - 1))
         current_turn = total_turn - base
-        num = n ** (order - 1) + (current_turn) // order
-        answer.append(convert(num + 1, n)[current_turn % order])
+        num = base + current_turn // order + 1
+        print(next, total_turn, current_turn, base)
+        # print(current_turn, total_turn, order)
+        # d, r = divmod(current_turn, order)
+        print('c', num, convert(num, n), 'base', base, current_turn // order, int(current_turn % order),
+              convert(num, n)[int(current_turn % order) - 1])
+        print(answer)
+        answer.append(convert(num, n)[int(current_turn % order)])
+        # print(answer)
         x += 1
-    return answer
+    print(answer)
+    return ''.join(answer)
 
 
 def test():
-    print(solution(2, 4, 2, 1))
-    print(solution(16, 16, 2, 1))
-    print(solution(16, 16, 2, 2))
+    # ans1 = solution(2, 4, 2, 1)
+    # ans1 = solution(2, 4, 2, 2)
+    ans2 = solution(16, 16, 2, 1)
+    ans3 = solution(16, 16, 2, 2)
 
 
 if __name__ == '__main__':
@@ -49,3 +62,30 @@ if __name__ == '__main__':
     # n, t, m, p = input(), input(), input(), input()
     # print(n, t, m, p)
     pass
+
+# 테스트 1 〉	통과 (0.03ms, 10.4MB)
+# 테스트 2 〉	통과 (0.08ms, 10.3MB)
+# 테스트 3 〉	통과 (0.06ms, 10.3MB)
+# 테스트 4 〉	통과 (0.04ms, 10.2MB)
+# 테스트 5 〉	통과 (0.16ms, 10.3MB)
+# 테스트 6 〉	실패 (0.15ms, 10.2MB)
+# 테스트 7 〉	실패 (0.15ms, 10.3MB)
+# 테스트 8 〉	통과 (0.10ms, 10.2MB)
+# 테스트 9 〉	실패 (0.10ms, 10.3MB)
+# 테스트 10 〉	실패 (0.10ms, 10.2MB)
+# 테스트 11 〉	통과 (0.10ms, 10.2MB)
+# 테스트 12 〉	실패 (0.09ms, 10.2MB)
+# 테스트 13 〉	실패 (0.09ms, 10.2MB)
+# 테스트 14 〉	통과 (3.08ms, 10.2MB)
+# 테스트 15 〉	실패 (3.11ms, 10.2MB)
+# 테스트 16 〉	실패 (3.71ms, 10.2MB)
+# 테스트 17 〉	실패 (0.62ms, 10.2MB)
+# 테스트 18 〉	실패 (0.76ms, 10.1MB)
+# 테스트 19 〉	실패 (0.53ms, 10.2MB)
+# 테스트 20 〉	실패 (0.24ms, 10.3MB)
+# 테스트 21 〉	실패 (1.24ms, 10.1MB)
+# 테스트 22 〉	실패 (2.14ms, 10.2MB)
+# 테스트 23 〉	실패 (3.57ms, 10.2MB)
+# 테스트 24 〉	실패 (3.95ms, 10.5MB)
+# 테스트 25 〉	실패 (3.58ms, 10.2MB)
+# 테스트 26 〉	실패 (1.68ms, 10.2MB)
--- kakao/2018/lv3/thanksgiving_traffic.py
+++ kakao/2018/lv3/thanksgiving_traffic.py
@@ -1,29 +1,65 @@
 # https://programmers.co.kr/learn/courses/30/lessons/17676
 import json
 from datetime import datetime as dt
+import time
+from collections import Counter
 
 
 def solution(lines):
-    lines = [(dt.strptime(' '.join(line.split()[0:2]), '%Y-%m-%d %H:%M:%S.%f'), float(line.split()[2][:-1])) for line in
-             lines]
-    print(lines)
-    answer = 0
+    # base = (dt.strptime('2016-09-15 20:59:57.421', '%Y-%m-%d %H:%M:%S.%f').timestamp())
+
+    # print('base', base)
+    lines = [
+        (int((dt.strptime(' '.join(line.split()[0:2]), '%Y-%m-%d %H:%M:%S.%f').timestamp()) * 1000),
+         int(float(line.split()[2][:-1]) * 1000)) for line in
+        lines]
+    d = dict()
+    for line in lines:
+        st = line[0] - line[1] + 1
+        et = line[0]
+        print(dt.fromtimestamp(st / 1000), dt.fromtimestamp(et / 1000))
+        for t in range(st, et + 1):
+            if t in d:
+                d[t] += 1
+            else:
+                d[t] = 1
+    answer = max(d.values())
+
+    print(min(d.values()))
+    print(Counter(d))
+    l = [key if d[key] == 5 else -1 for key in d]
+    print(l)
+
+    print(dt.fromtimestamp(1473940799.447))
+    print(dt.fromtimestamp(1473940799.591))
+    # print(dt.fromtimestamp(1473940797))
     return answer
 
 
 def test():
-    print(solution([
-        "2016-09-15 20:59:57.421 0.351s",
-        "2016-09-15 20:59:58.233 1.181s",
-        "2016-09-15 20:59:58.299 0.8s",
-        "2016-09-15 20:59:58.688 1.041s",
-        "2016-09-15 20:59:59.591 1.412s",
-        "2016-09-15 21:00:00.464 1.466s",
-        "2016-09-15 21:00:00.741 1.581s",
-        "2016-09-15 21:00:00.748 2.31s",
-        "2016-09-15 21:00:00.966 0.381s",
-        "2016-09-15 21:00:02.066 2.62s"
-    ]))
+    # print(solution([
+    #     "2016-09-15 20:59:57.421 0.351s",
+    #     "2016-09-15 20:59:58.233 1.181s",
+    #     "2016-09-15 20:59:58.299 0.8s",
+    #     "2016-09-15 20:59:58.688 1.041s",
+    #     "2016-09-15 20:59:59.591 1.412s",
+    #     "2016-09-15 21:00:00.464 1.466s",
+    #     "2016-09-15 21:00:00.741 1.581s",
+    #     "2016-09-15 21:00:00.748 2.31s",
+    #     "2016-09-15 21:00:00.966 0.381s",
+    #     "2016-09-15 21:00:02.066 2.62s"
+    # ]))
+
+    print(solution(["2016-09-15 20:59:57.421 0.351s",
+                    "2016-09-15 20:59:58.233 1.181s",
+                    "2016-09-15 20:59:58.299 0.8s",
+                    "2016-09-15 20:59:58.688 1.041s",
+                    "2016-09-15 20:59:59.591 1.412s",
+                    "2016-09-15 21:00:00.464 1.466s",
+                    "2016-09-15 21:00:00.741 1.581s",
+                    "2016-09-15 21:00:00.748 2.31s",
+                    "2016-09-15 21:00:00.966 0.381s",
+                    "2016-09-15 21:00:02.066 2.62s"]))
 
 
 if __name__ == '__main__':
