430aa17fd38c8324ee215af3d6ff6a71b81310c2
wooilkim <wooilkim@korea.ac.kr>
2021-03-18 19:48:55 +0900

    input processing

--- samsung/12100_2048(Easy).py
+++ samsung/12100_2048(Easy).py
@@ -1,1 +1,19 @@
 # https://www.acmicpc.net/problem/12100
+
+LEFT, RIGHT, UP, DOWN = 0, 1, 2, 3
+
+
+def solution(n, arr):
+    print(arr)
+
+
+def move(d):
+    pass
+
+
+if __name__ == '__main__':
+    n = int(input())
+    arr = list()
+    for i in range(n):
+        arr.append(list(map(int, input().split())))
+    solution(n, arr)
--- samsung/3190_snake.py
+++ samsung/3190_snake.py
@@ -1,9 +1,19 @@
 # https://www.acmicpc.net/problem/3190
 
 
-def solution():
+def solution(n, k, apples, l, dirs):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    n = int(input())
+    k = int(input())
+    apples = list()
+    for row in range(k):
+        apples.append(list(map(int, (input().split()))))
+    l = int(input())
+    dirs = list()
+    for row in range(l):
+        dirs.append(input().split())
+        dirs[row][0] = int(dirs[row][0])
+    print(solution(n, k, apples, l, dirs))
