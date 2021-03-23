60bb3f395847d60579383fa5795c6ffa2eee0f23
wooilkim <wooilkim@korea.ac.kr>
2021-03-23 18:46:30 +0900

    other's attached

--- samsung/14502_lab.py
+++ samsung/14502_lab.py
@@ -62,3 +62,58 @@ if __name__ == '__main__':
     for i in range(n):
         board.append(list(map(int, input().split())))
     solution(n, m, board)
+
+
+# import copy
+# from collections import deque
+#
+# n,m = map(int, input().split())
+# dir_list = [[1, 0], [-1, 0],[0, 1],[0, -1]]
+# graph = []
+# max_zero_cnt = 0
+# for _ in range(n):
+#     graph.append(list(map(int, input().strip().split())))
+#
+# def check_zero_cnt(graph):
+#     global n, m
+#     cnt = 0
+#     for i in range(n):
+#         for j in range(m):
+#             if graph[i][j] == 0:
+#                 cnt += 1
+#     return cnt
+#
+# def make_virus_fill(graph):
+#     global n, m
+#     dq = deque()
+#     for i in range(n):
+#         for j in range(m):
+#             if graph[i][j] == 2:
+#                 dq.append((j, i)) # x, y
+#
+#     while dq:
+#         x, y = dq.popleft()
+#         for dir in dir_list:
+#             dx = dir[0]
+#             dy = dir[1]
+#             next_x = x+dx
+#             next_y = y+dy
+#             if 0 <= next_x < m and 0 <= next_y < n and graph[next_y][next_x] == 0:
+#                 graph[next_y][next_x] = 2
+#                 dq.append((next_x, next_y))
+#     return check_zero_cnt(graph)
+#
+# def make_map_dfs(graph, cnt):
+#     global n, m, max_zero_cnt
+#     if cnt == 3:
+#         max_zero_cnt = max(max_zero_cnt, make_virus_fill(graph))
+#     elif cnt < 3:
+#         for i in range(n):
+#             for j in range(m):
+#                 if graph[i][j] == 0:
+#                     tmp_graph = copy.deepcopy(graph)
+#                     tmp_graph[i][j] = 1
+#                     make_map_dfs(tmp_graph, cnt+1)
+#
+# make_map_dfs(graph, 0)
+# print(max_zero_cnt)
