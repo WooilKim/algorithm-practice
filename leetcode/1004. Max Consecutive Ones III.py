2ea073173ce0f9edb2ef51018ee935239ed28359
wooilkim <wooilkim@korea.ac.kr>
2021-03-24 10:50:42 +0900

    solution added

--- leetcode/1004. Max Consecutive Ones III.py
+++ leetcode/1004. Max Consecutive Ones III.py
@@ -1,34 +1,59 @@
 # https://leetcode.com/problems/max-consecutive-ones-iii/
 
-class Solution:
-    def longestOnes(self, A: list[int], K: int) -> int:
-        l = [[], []]
-        flag = 1
-        cnt = 0
-        for i, a in enumerate(A):
-            if a == flag:
-                cnt += 1
-            else:
-                l[flag].append(cnt)
-                flag = a
-                cnt = 1
-        l[A[-1]].append(cnt)
-        M = l[1][0]
-        for i in range(1, len(l[1])):
-            j = i
-            k_left = K
-            while k_left > 0 and j > 0:
-                k_left -= l[0][j - 1]
-                j -= 1
-            if k_left >= 0:
-                if len(l[0]) > i:
-                    s = sum(l[1][j:i + 1]) + K - max(0, k_left - l[0][i])
-                else:
-                    s = sum(l[1][j:i + 1]) + K - k_left
-            else:
-                s = sum(l[1][j + 1:i + 1]) + K
-            M = max(M, s)
-        return M
+class Solution(object):
+    def longestOnes(self, A, K):
+        """
+        :type A: List[int]
+        :type K: int
+        :rtype: int
+        """
+        zero_indices = [-1]
+        for i, num in enumerate(A):
+            if num == 0:
+                zero_indices.append(i)
+        zero_indices.append(len(A))
+
+        if K >= len(zero_indices) - 2:
+            return len(A)
+
+        maxZeros = 0
+
+        for i in range(K, len(zero_indices) - 1):
+            zeros = zero_indices[i + 1] - zero_indices[i - K] - 1
+            maxZeros = max(zeros, maxZeros)
+
+        return maxZeros
+
+
+# class Solution:
+#     def longestOnes(self, A: list[int], K: int) -> int:
+#         l = [[], []]
+#         flag = 1
+#         cnt = 0
+#         for i, a in enumerate(A):
+#             if a == flag:
+#                 cnt += 1
+#             else:
+#                 l[flag].append(cnt)
+#                 flag = a
+#                 cnt = 1
+#         l[A[-1]].append(cnt)
+#         M = l[1][0]
+#         for i in range(1, len(l[1])):
+#             j = i
+#             k_left = K
+#             while k_left > 0 and j > 0:
+#                 k_left -= l[0][j - 1]
+#                 j -= 1
+#             if k_left >= 0:
+#                 if len(l[0]) > i:
+#                     s = sum(l[1][j:i + 1]) + K - max(0, k_left - l[0][i])
+#                 else:
+#                     s = sum(l[1][j:i + 1]) + K - k_left
+#             else:
+#                 s = sum(l[1][j + 1:i + 1]) + K
+#             M = max(M, s)
+#         return M
 
 
 print(Solution().longestOnes(A=[1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], K=2))
