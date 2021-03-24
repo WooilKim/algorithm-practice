b3c23f42fec3913ecee5c17ce940e8d51a0fe76a
wooilkim <wooilkim@korea.ac.kr>
2021-03-24 10:29:21 +0900

    cleaned Runtime: 2664 ms, faster than 5.15% of Python3 online submissions for Max Consecutive Ones III. Memory Usage: 15 MB, less than 11.71% of Python3 online submissions for Max Consecutive Ones III.

--- leetcode/1004. Max Consecutive Ones III.py
+++ leetcode/1004. Max Consecutive Ones III.py
@@ -2,28 +2,19 @@
 
 class Solution:
     def longestOnes(self, A: list[int], K: int) -> int:
-        M = 0
         l = [[], []]
-
-        zeros = list()
-        ones = list()
         flag = 1
         cnt = 0
         for i, a in enumerate(A):
-            if a != flag:
+            if a == flag:
+                cnt += 1
+            else:
                 l[flag].append(cnt)
                 flag = a
                 cnt = 1
-            else:
-                cnt += 1
         l[A[-1]].append(cnt)
-        print(l)
-        print(len(l[0]), len(l[1]))
-
-        for i in range(len(l[1])):
-            if i == 0:
-                M = l[1][0]
-                continue
+        M = l[1][0]
+        for i in range(1, len(l[1])):
             j = i
             k_left = K
             while k_left > 0 and j > 0:
@@ -36,8 +27,10 @@ class Solution:
                     s = sum(l[1][j:i + 1]) + K - k_left
             else:
                 s = sum(l[1][j + 1:i + 1]) + K
-            print(j, i, s, k_left)
             M = max(M, s)
         return M
 
 
+print(Solution().longestOnes(A=[1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], K=2))
+print(Solution().longestOnes(A=[0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1], K=3))
+print(Solution().longestOnes(A=[0, 0, 0, 1], K=4))
