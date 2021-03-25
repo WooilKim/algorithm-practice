38df27866e4e7d2b0c6407ad693cfa2e6522e55f
wooilkim <wooilkim@korea.ac.kr>
2021-03-25 20:40:37 +0900

    in progress

--- samsung/14889_start_link.py
+++ samsung/14889_start_link.py
@@ -1,1 +1,28 @@
 # https://www.acmicpc.net/problem/14889
+from itertools import combinations
+
+
+class Solution:
+    def min_diff(self, n, S: list[list[int]]) -> int:
+        global m
+        m = 9999
+        # for i in range(n):
+        #     for j in range(i, n):
+        #         S[i][j], S[j][i] = S[i][j] + S[j][i], S[i][j] + S[j][i]
+        # print(list(combinations(n,2)))
+        scores = [S[a][b] + S[b][a] for a, b in combinations(range(n), 2)]
+        size = len(scores)
+        m = min([sum(c) for c in combinations(scores, size // 2)])
+        print(m)
+        print(scores)
+        cs = combinations(range(n), n // 2)
+        print(list(cs))
+        for c in cs:
+            pass
+
+
+if __name__ == '__main__':
+    n = int(input())
+    S = [list(map(int, input().split())) for i in range(n)]
+    # print(S)
+    print(Solution().min_diff(n, S))
