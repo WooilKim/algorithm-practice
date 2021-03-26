af1866ac378b5236b475a3c0eecc82baff735c0a
wooilkim <wooilkim@korea.ac.kr>
2021-03-26 17:09:35 +0900

    code cleaned

--- samsung/14889_start_link.py
+++ samsung/14889_start_link.py
@@ -6,27 +6,21 @@ class Solution:
     def min_diff(self, n, S: list[list[int]]) -> int:
         global m
         m = 9999
-        # for i in range(n):
-        #     for j in range(i, n):
-        #         S[i][j], S[j][i] = S[i][j] + S[j][i], S[i][j] + S[j][i]
-        # print(list(combinations(n,2)))
-        scores = [S[a][b] + S[b][a] for a, b in combinations(range(n), 2)]
         cs = list(x for x in combinations(range(n), n // 2) if 0 in x)
-        print(cs)
-
-        # a 가 포함된것만 하면 반이됨
-        size = len(scores)
-        m = min([sum(c) for c in combinations(scores, size // 2)])
-        print(m)
-        print(scores)
-        cs = combinations(range(n), n // 2)
-        print(list(cs))
         for c in cs:
-            pass
+            partsumA = 0
+            partsumB = 0
+            for a, b in combinations(c, 2):
+                partsumA += S[a][b] + S[b][a]
+            for a, b in combinations([x for x in range(n) if x not in c], 2):
+                partsumB += S[a][b] + S[b][a]
+
+            m = min(m, abs(partsumB - partsumA))
+
+        return m
 
 
 if __name__ == '__main__':
     n = int(input())
     S = [list(map(int, input().split())) for i in range(n)]
-    # print(S)
     print(Solution().min_diff(n, S))
