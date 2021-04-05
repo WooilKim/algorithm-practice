e9b3e5d030acf173c035543a37d305417591b01c
wooilkim <wooilkim@korea.ac.kr>
2021-04-05 13:23:06 +0900

    solution added

--- leetcode/503. Greater Element II.py
+++ leetcode/503. Greater Element II.py
@@ -1,8 +1,26 @@
 # https://leetcode.com/problems/next-greater-element-ii/
 
+# Runtime: 280 ms, faster than 19.63% of Python3 online submissions for Next Greater Element II.
+# Memory Usage: 15.7 MB, less than 62.83% of Python3 online submissions for Next Greater Element II.
+class Solution:
+    def nextGreaterElements(self, nums: list[int]) -> list[int]:
+        stack = nums[::-1]
+        ans = [-1 for _ in nums]
+        for i in range(len(nums) * 2)[::-1]:
+            while stack and stack[-1] <= nums[i % len(nums)]:
+                stack.pop()
+            else:
+                if stack:
+                    ans[i % len(nums)] = stack[-1]
+                else:
+                    ans[i % len(nums)] = -1
+                stack.append(nums[i % len(nums)])
+        return ans
+
+
 # Runtime: 8068 ms, faster than 5.04% of Python3 online submissions for Next Greater Element II.
 # Memory Usage: 15.7 MB, less than 83.91% of Python3 online submissions for Next Greater Element II.
-class Solution:
+class Solution2:
     def nextGreaterElements(self, nums: list[int]) -> list[int]:
         ans = [-1 for _ in nums]
         for i in range(len(nums)):
