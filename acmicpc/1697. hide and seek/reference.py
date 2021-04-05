555189603dc887c8b35a27de9e467528c6020e78
wooilkim <wooilkim@korea.ac.kr>
2021-04-05 19:32:39 +0900

    path modified

--- acmicpc/1697. hide and seek/reference.py
+++
@@ -1,5 +0,0 @@
-# Runtime: 52 ms, faster than 95.68% of Python3 online submissions for Triangle.
-# Memory Usage: 15 MB, less than 86.86% of Python3 online submissions for Triangle.
-class Solution1:
-    pass
-
--- acmicpc/samsung/15685/README.md
+++
@@ -1,4 +0,0 @@
-# Problem name
-
-## History
-
