610160a94e3b8e169749d3805fca44988857bbb6
wooilkim <wooilkim@korea.ac.kr>
2021-04-11 22:24:13 +0900

    in progress

---
+++ leetcode/Amazon/Arrays_and_Strings/10.Rotate_Image.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2969/
+from typing import List
+
+
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """
