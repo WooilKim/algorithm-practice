6eb44a97ea28e5edd52fed0b6ee12e161034c225
wooilkim <wooilkim@korea.ac.kr>
2021-04-12 16:58:08 +0900

    in progress

--- leetcode/Amazon/Arrays_and_Strings/10.Rotate_Image.py
+++ leetcode/Amazon/Arrays_and_Strings/10.Rotate_Image.py
@@ -7,3 +7,14 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
+        n = len(matrix[0])
+        for i in range(n // 2 + n % 2):
+            for j in range(n // 2):
+                matrix[n - 1 - j][i], matrix[n - 1 - i][n - j - 1], matrix[j][n - 1 - i] \
+                    = matrix[n - 1 - i][n - j - 1], matrix[j][n - 1 - i], matrix[i][j]
+
+
+if __name__ == '__main__':
+    Solution().rotate(matrix=[[1, 2, 3],
+                              [4, 5, 6],
+                              [7, 8, 9]])
