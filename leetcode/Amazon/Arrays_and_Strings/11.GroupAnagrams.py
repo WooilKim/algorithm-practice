4a8985bc45d0867b69579a33337d98614b7ab73f
wooilkim <wooilkim@korea.ac.kr>
2021-04-13 12:36:56 +0900

    problem added

---
+++ leetcode/Amazon/Arrays_and_Strings/11.GroupAnagrams.py
@@ -0,0 +1,14 @@
+# https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2970/
+
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = list()
+
+        return ans
+
+
+if __name__ == '__main__':
+    print(Solution().groupAnagrams(strs=["eat", "tea", "tan", "ate", "nat", "bat"]))
