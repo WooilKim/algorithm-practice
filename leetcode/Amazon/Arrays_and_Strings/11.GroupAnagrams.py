2a519ddd28d86e378af100473ef6dd4876a7e251
wooilkim <wooilkim@korea.ac.kr>
2021-04-13 16:28:27 +0900

    solved

--- leetcode/Amazon/Arrays_and_Strings/11.GroupAnagrams.py
+++ leetcode/Amazon/Arrays_and_Strings/11.GroupAnagrams.py
@@ -1,13 +1,18 @@
 # https://leetcode.com/explore/interview/card/amazon/76/array-and-strings/2970/
 
 from typing import List
+from collections import defaultdict
 
 
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        ans = list()
-
-        return ans
+        ans = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            ans[tuple(count)].append(s)
+        return ans.values()
 
 
 if __name__ == '__main__':
