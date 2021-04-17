b504f18eb906d5fefaf88fa089b5f660cc9bfee2
wooilkim <wooilkim@korea.ac.kr>
2021-04-18 00:39:26 +0900

    euler problem table

--- summarize.py
+++ summarize.py
@@ -8,6 +8,17 @@ def summarize():
         f.write('# Algorithm Practice\n')
         for site in sites:
             f.write(f'## {site}\n')
+            if site == 'projecteuler':
+                num_problem = 755  # 2021/04/18 update
+                ps = [i for i in range(1, num_problem + 1)]
+                ps = [' | '.join(map(str, ps[i * 10:i * 10 + 10])) for i in range(num_problem // 10 + 1)]
+                f.write("""| | | | | | | | | | |
+|---|---|---|---|---|---|---|---|---|---|
+""")
+                for p in ps:
+                    f.write(f'| {p} |\n')
+                # print('\n'.join(ps))
+
             path = os.walk(f"./{site}")
             for root, directories, files in path:
                 print('directories', directories)
