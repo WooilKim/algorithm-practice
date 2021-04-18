3c9ca093f948e3cf92c76e84bc6e5b762276c202
wooilkim <wooilkim@korea.ac.kr>
2021-04-18 22:07:56 +0900

    solved

--- projecteuler/21.AmicableNumbers.py
+++ projecteuler/21.AmicableNumbers.py
@@ -7,12 +7,21 @@ The proper divisors of 284 are 1, 2, 4, 71 and 142; so d(284) = 220.
 
 Evaluate the sum of all the amicable numbers under 10000.
 """
+from collections import defaultdict
 
 
 def amicable_numbers(n):
-    for i in range(1, n+1):
-
-    return
+    nums = defaultdict(list)
+    ans = list()
+    for i in range(1, n + 1):
+        nums[d(i)].append(i)
+    for i in range(1, n + 1):
+        for nu in nums[i]:
+            if nu in nums[i] and nu in nums and i in nums[nu] and i != nu and i not in ans:
+                ans.append(nu)
+                ans.append(i)
+                break
+    return sum(ans), ans
 
 
 def d(n):
