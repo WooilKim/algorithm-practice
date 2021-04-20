d1d5134297ec0f7c6e35be5f62cb9ad7dee12c86
wooilkim <wooilkim@korea.ac.kr>
2021-04-20 20:33:56 +0900

    solved

---
+++ leetcode/268.MissingNumber.py
@@ -0,0 +1,12 @@
+# https://leetcode.com/problems/missing-number/
+
+from typing import List
+
+
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        pass
+
+
+if __name__ == '__main__':
+    nums = [3, 0, 1]
