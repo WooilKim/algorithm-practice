d5ab5a218a3767d9e263b0a9daa89e21aa433219
wooilkim <wooilkim@korea.ac.kr>
2021-05-06 11:54:00 +0900

    solved

--- leetcode/268.MissingNumber.py
+++ leetcode/268.MissingNumber.py
@@ -3,9 +3,17 @@
 from typing import List
 
 
+class Solution2:
+    def missingNumber(self, nums):
+        missing = len(nums)
+        for i, num in enumerate(nums):
+            missing ^= i ^ num
+        return missing
+
+
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        pass
+        return len(nums) * (len(nums) + 1) // 2 - sum(nums)
 
 
 if __name__ == '__main__':
