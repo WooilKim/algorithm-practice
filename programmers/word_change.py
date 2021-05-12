f9b39fc3e8b814ea664f7107a94f2a62b9f84420
wooilkim <wooilkim@korea.ac.kr>
2021-05-12 16:52:56 +0900

    solved

---
+++ programmers/word_change.py
@@ -0,0 +1,37 @@
+# https://programmers.co.kr/learn/courses/30/lessons/43163
+import math
+from collections import deque
+
+
+def get_adjacent(current, words):
+    for word in words:
+        if len(current) != len(word):
+            continue
+
+        count = 0
+        for c, w in zip(current, word):
+            if c != w:
+                count += 1
+
+        if count == 1:
+            yield word
+
+
+def solution(begin, target, words):
+    dist = {begin: 0}
+    queue = deque([begin])
+
+    while queue:
+        current = queue.popleft()
+
+        for next_word in get_adjacent(current, words):
+            if next_word not in dist:
+                dist[next_word] = dist[current] + 1
+                queue.append(next_word)
+
+    return dist.get(target, 0)
+
+
+if __name__ == '__main__':
+    print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log", "cog"]), 4)
+    print(solution("hit", "cog", ["hot", "dot", "dog", "lot", "log"]), 0)
