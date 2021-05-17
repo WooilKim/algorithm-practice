09ba6a3f40bd5f2fe7520cd82b48f2957717a870
wooilkim <wooilkim@korea.ac.kr>
2021-05-17 16:07:43 +0900

    init

---
+++ leetcode/1235.MaximumProfitInJobScheduling.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/maximum-profit-in-job-scheduling/
+
+from typing import List
+
+
+class Solution:
+    def jobScheduling(self, startTime: List[int], endTime: List[int], profit: List[int]) -> int:
+
+        pass
+
+
+if __name__ == '__main__':
+    print(Solution().jobScheduling(startTime=[1, 2, 3, 3], endTime=[3, 4, 5, 6], profit=[50, 10, 40, 70]))
