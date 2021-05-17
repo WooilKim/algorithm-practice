39acee72a1a9f0a03c78ee064c13677c9505f0bc
wooilkim <wooilkim@korea.ac.kr>
2021-05-17 16:07:54 +0900

    TODO: Wrong answer

---
+++ leetcode/5.LongestPalindromicSubstring.py
@@ -0,0 +1,30 @@
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+from collections import deque
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def search(l, r):
+            ans = ''
+            while l >= 0 and r < len(s):
+                if s[l] == s[r]:
+                    ans = s[l:r + 1]
+                l -= 1
+                r += 1
+            return ans
+
+        res = list()
+        for i in range(len(s)):
+            res.append(search(i, i))
+            if i < len(s) - 1:
+                res.append(search(i, i + 1))
+        return [x for x in sorted(res, key=lambda x: len(x))][-1]
+
+
+if __name__ == '__main__':
+    print(Solution().longestPalindrome("babad"))
+    print(Solution().longestPalindrome("aba"))
+    print(Solution().longestPalindrome(
+        "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"))
+    print(Solution().longestPalindrome("aacabdkacaa"))
