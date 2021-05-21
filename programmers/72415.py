f82d2c109d646f71fedf8c983f259d5fbda1673f
wooilkim <wooilkim@korea.ac.kr>
2021-05-22 00:46:45 +0900

    init

---
+++ programmers/72415.py
@@ -0,0 +1,15 @@
+# https://programmers.co.kr/learn/courses/30/lessons/72415
+# title : 카드 짝 맞추기
+
+def solution(board, r, c):
+    answer = 0
+
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([[1, 0, 0, 3], [2, 0, 0, 0], [0, 0, 0, 2], [3, 0, 1, 0]], 1, 0), 14)
+
+"""
+0 <= board[i][j] <= 3 
+"""
---
+++ programmers/72416.py
@@ -0,0 +1,12 @@
+# https://programmers.co.kr/learn/courses/30/lessons/72416
+# title : 매출 하락 최소화
+
+
+def solution(sales, links):
+    answer = 0
+    return answer
+
+
+if __name__ == '__main__':
+    print(solution([14, 17, 15, 18, 19, 14, 13, 16, 28, 17],
+                   [[10, 8], [1, 9], [9, 7], [5, 4], [1, 5], [5, 10], [10, 6], [1, 3], [10, 2]]), 44)
