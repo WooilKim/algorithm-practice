0805c14beb45c1beca8d8d89f1c22c29ebde60a7
wooilkim <wooilkim@korea.ac.kr>
2021-05-25 19:33:28 +0900

    solved

---
+++ check.io/home/11.SplitPairs.py
@@ -0,0 +1,15 @@
+def split_pairs(a):
+    return [a[i:i + 2] if i + 1 < len(a) else a[i] + '_' for i in range(0, len(a), 2)]
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(list(split_pairs('abcd')))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert list(split_pairs('abcd')) == ['ab', 'cd']
+    assert list(split_pairs('abc')) == ['ab', 'c_']
+    assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
+    assert list(split_pairs('a')) == ['a_']
+    assert list(split_pairs('')) == []
+    print("Coding complete? Click 'Check' to earn cool rewards!")
