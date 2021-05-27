451f295466b22c38729cbfeec71f4ce474678561
wooilkim <wooilkim@korea.ac.kr>
2021-05-27 11:22:19 +0900

    solved 212

--- leetcode/212.Word_Search_II.py
+++ leetcode/212.Word_Search_II.py
@@ -1,63 +1,165 @@
 # https://leetcode.com/problems/word-search-ii/
 # 212. Word Search II
 
+from collections import deque
 
-# Runtime: 164 ms, faster than 87.37% of Python3 online submissions for Kth Smallest Element in a Sorted Matrix.
-# Memory Usage: 20.1 MB, less than 49.41% of Python3 online submissions for Kth Smallest Element in a Sorted Matrix.
+
+class Node:
+    def __init__(self, key):
+        self.key = key
+        self.child = dict()
+
+
+class Trie:
+    def __init__(self):
+        self.root = Node(None)
+
+    def insert(self, word):
+        cur = self.root
+        for w in word:
+            if w not in cur.child:
+                cur.child[w] = Node(w)
+            cur = cur.child[w]
+        cur.child['*'] = True
+
+    def search(self, word):
+        cur = self.root
+        for w in word:
+            if w not in cur.child:
+                return False
+            cur = cur.child[w]
+        if '*' in cur.child:
+            return True
+
+
+# Runtime: 9860 ms, faster than 5.02% of Python3 online submissions for Word Search II.
+# Memory Usage: 14.7 MB, less than 40.62% of Python3 online submissions for Word Search II.
 class Solution:
     def findWords(self, board: list[list[str]], words: list[str]) -> list[str]:
         m, n = len(board[0]), len(board)
-        answer = list()
-        for word in words:
-            stack = list()
-            visited = list()
-            start_coords = [(j, i) for i in range(m) for j in range(n) if board[j][i] == word[0]]
-            i = 0
-            for coord in start_coords:
-                stack.append((i, coord))
-            # print('stack', stack)
-            while stack:
-                item = stack.pop()
-                if item[0] == len(word) - 1:
-                    answer.append(word)
-                    break
-                # print('item', item)
+        answer = set()
+        trie = Trie()
 
-                item_y, item_x = item[1][0], item[1][1]
-                # print(m, n, item_y, item_x)
-                if item_y < n - 1 and board[item_y + 1][item_x] == word[i + 1]:
-                    if (item_y + 1, item_x) not in [v[1] for v in visited]:
-                        stack.append((i + 1, (item_y + 1, item_x)))
-                if item_y > 0 and board[item_y - 1][item_x] == word[i + 1]:
-                    if (item_y - 1, item_x) not in [v[1] for v in visited]:
-                        stack.append((i + 1, (item_y - 1, item_x)))
-                if item_x < m - 1 and board[item_y][item_x + 1] == word[i + 1]:
-                    if (item_y, item_x + 1) not in [v[1] for v in visited]:
-                        stack.append((i + 1, (item_y, item_x + 1)))
-                if item_x > 0 and board[item_y][item_x - 1] == word[i + 1]:
-                    if (item_y, item_x - 1) not in [v[1] for v in visited]:
-                        stack.append((i + 1, (item_y, item_x - 1)))
-                print('stack after', stack)
-                if stack:
-                    if stack[len(stack) - 1][0] == item[0] + 1:
-                        visited.append(item)
-                        i += 1
-                else:
-                    if visited:
-                        visited.pop()
-                    i -= 1
-                print('visited', visited)
-        return sorted(answer)
+        def dfs(node, y, x, word, visited):
+            if '*' in node.child:
+                answer.add(word)
+            dy = [1, 0, -1, 0]
+            dx = [0, 1, 0, -1]
+            for d in range(4):
+                ny, nx = y + dy[d], x + dx[d]
+                if 0 <= ny < n and 0 <= nx < m and (ny, nx) not in visited:
+                    if board[ny][nx] in node.child:
+                        dfs(node.child[board[ny][nx]], ny, nx, word + board[ny][nx], visited + [(ny, nx)])
+
+        for word in words:
+            trie.insert(word)
+        for j in range(n):
+            for i in range(m):
+                curr = trie.root
+                c = board[j][i]
+                if c in curr.child:
+                    dfs(curr.child[c], j, i, c, [(j, i)])
+        return sorted(list(answer))
 
 
 if __name__ == '__main__':
     # print(Solution().findWords(board=[["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"],
     #                                   ["i", "f", "l", "v"]], words=["oath", "eat", "pea", "rain"]))
     # print(Solution().findWords(board=[["a", "a"]], words=["aa"]))
-    # print(Solution().findWords([["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"], ["i", "f", "l", "v"]],
-    #                            ["oath", "pea", "eat", "rain"]))
+    print(Solution().findWords([["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"], ["i", "f", "l", "v"]],
+                               ["oath", "pea", "eat", "rain"]))
     # print(Solution().findWords([["a", "a"]], ["aaa"]))
-    print(Solution().findWords([["a", "a"], ["a", "a"]], ["aaaaa"]))
-
+    # print(Solution().findWords([["a", "a"], ["a", "a"]], ["aaaaa"]))
+    print(Solution().findWords(
+        [["b", "a", "b", "a", "b", "a", "b", "a", "b", "a"], ["a", "b", "a", "b", "a", "b", "a", "b", "a", "b"],
+         ["b", "a", "b", "a", "b", "a", "b", "a", "b", "a"], ["a", "b", "a", "b", "a", "b", "a", "b", "a", "b"],
+         ["b", "a", "b", "a", "b", "a", "b", "a", "b", "a"], ["a", "b", "a", "b", "a", "b", "a", "b", "a", "b"],
+         ["b", "a", "b", "a", "b", "a", "b", "a", "b", "a"], ["a", "b", "a", "b", "a", "b", "a", "b", "a", "b"],
+         ["b", "a", "b", "a", "b", "a", "b", "a", "b", "a"], ["a", "b", "a", "b", "a", "b", "a", "b", "a", "b"]],
+        ["ababababaa", "ababababab", "ababababac", "ababababad", "ababababae", "ababababaf", "ababababag", "ababababah",
+         "ababababai", "ababababaj", "ababababak", "ababababal", "ababababam", "ababababan", "ababababao", "ababababap",
+         "ababababaq", "ababababar", "ababababas", "ababababat", "ababababau", "ababababav", "ababababaw", "ababababax",
+         "ababababay", "ababababaz", "ababababba", "ababababbb", "ababababbc", "ababababbd", "ababababbe", "ababababbf",
+         "ababababbg", "ababababbh", "ababababbi", "ababababbj", "ababababbk", "ababababbl", "ababababbm", "ababababbn",
+         "ababababbo", "ababababbp", "ababababbq", "ababababbr", "ababababbs", "ababababbt", "ababababbu", "ababababbv",
+         "ababababbw", "ababababbx", "ababababby", "ababababbz", "ababababca", "ababababcb", "ababababcc", "ababababcd",
+         "ababababce", "ababababcf", "ababababcg", "ababababch", "ababababci", "ababababcj", "ababababck", "ababababcl",
+         "ababababcm", "ababababcn", "ababababco", "ababababcp", "ababababcq", "ababababcr", "ababababcs", "ababababct",
+         "ababababcu", "ababababcv", "ababababcw", "ababababcx", "ababababcy", "ababababcz", "ababababda", "ababababdb",
+         "ababababdc", "ababababdd", "ababababde", "ababababdf", "ababababdg", "ababababdh", "ababababdi", "ababababdj",
+         "ababababdk", "ababababdl", "ababababdm", "ababababdn", "ababababdo", "ababababdp", "ababababdq", "ababababdr",
+         "ababababds", "ababababdt", "ababababdu", "ababababdv", "ababababdw", "ababababdx", "ababababdy", "ababababdz",
+         "ababababea", "ababababeb", "ababababec", "ababababed", "ababababee", "ababababef", "ababababeg", "ababababeh",
+         "ababababei", "ababababej", "ababababek", "ababababel", "ababababem", "ababababen", "ababababeo", "ababababep",
+         "ababababeq", "ababababer", "ababababes", "ababababet", "ababababeu", "ababababev", "ababababew", "ababababex",
+         "ababababey", "ababababez", "ababababfa", "ababababfb", "ababababfc", "ababababfd", "ababababfe", "ababababff",
+         "ababababfg", "ababababfh", "ababababfi", "ababababfj", "ababababfk", "ababababfl", "ababababfm", "ababababfn",
+         "ababababfo", "ababababfp", "ababababfq", "ababababfr", "ababababfs", "ababababft", "ababababfu", "ababababfv",
+         "ababababfw", "ababababfx", "ababababfy", "ababababfz", "ababababga", "ababababgb", "ababababgc", "ababababgd",
+         "ababababge", "ababababgf", "ababababgg", "ababababgh", "ababababgi", "ababababgj", "ababababgk", "ababababgl",
+         "ababababgm", "ababababgn", "ababababgo", "ababababgp", "ababababgq", "ababababgr", "ababababgs", "ababababgt",
+         "ababababgu", "ababababgv", "ababababgw", "ababababgx", "ababababgy", "ababababgz", "ababababha", "ababababhb",
+         "ababababhc", "ababababhd", "ababababhe", "ababababhf", "ababababhg", "ababababhh", "ababababhi", "ababababhj",
+         "ababababhk", "ababababhl", "ababababhm", "ababababhn", "ababababho", "ababababhp", "ababababhq", "ababababhr",
+         "ababababhs", "ababababht", "ababababhu", "ababababhv", "ababababhw", "ababababhx", "ababababhy", "ababababhz",
+         "ababababia", "ababababib", "ababababic", "ababababid", "ababababie", "ababababif", "ababababig", "ababababih",
+         "ababababii", "ababababij", "ababababik", "ababababil", "ababababim", "ababababin", "ababababio", "ababababip",
+         "ababababiq", "ababababir", "ababababis", "ababababit", "ababababiu", "ababababiv", "ababababiw", "ababababix",
+         "ababababiy", "ababababiz", "ababababja", "ababababjb", "ababababjc", "ababababjd", "ababababje", "ababababjf",
+         "ababababjg", "ababababjh", "ababababji", "ababababjj", "ababababjk", "ababababjl", "ababababjm", "ababababjn",
+         "ababababjo", "ababababjp", "ababababjq", "ababababjr", "ababababjs", "ababababjt", "ababababju", "ababababjv",
+         "ababababjw", "ababababjx", "ababababjy", "ababababjz", "ababababka", "ababababkb", "ababababkc", "ababababkd",
+         "ababababke", "ababababkf", "ababababkg", "ababababkh", "ababababki", "ababababkj", "ababababkk", "ababababkl",
+         "ababababkm", "ababababkn", "ababababko", "ababababkp", "ababababkq", "ababababkr", "ababababks", "ababababkt",
+         "ababababku", "ababababkv", "ababababkw", "ababababkx", "ababababky", "ababababkz", "ababababla", "abababablb",
+         "abababablc", "ababababld", "abababable", "abababablf", "abababablg", "abababablh", "ababababli", "abababablj",
+         "abababablk", "ababababll", "abababablm", "ababababln", "abababablo", "abababablp", "abababablq", "abababablr",
+         "ababababls", "abababablt", "abababablu", "abababablv", "abababablw", "abababablx", "abababably", "abababablz",
+         "ababababma", "ababababmb", "ababababmc", "ababababmd", "ababababme", "ababababmf", "ababababmg", "ababababmh",
+         "ababababmi", "ababababmj", "ababababmk", "ababababml", "ababababmm", "ababababmn", "ababababmo", "ababababmp",
+         "ababababmq", "ababababmr", "ababababms", "ababababmt", "ababababmu", "ababababmv", "ababababmw", "ababababmx",
+         "ababababmy", "ababababmz", "ababababna", "ababababnb", "ababababnc", "ababababnd", "ababababne", "ababababnf",
+         "ababababng", "ababababnh", "ababababni", "ababababnj", "ababababnk", "ababababnl", "ababababnm", "ababababnn",
+         "ababababno", "ababababnp", "ababababnq", "ababababnr", "ababababns", "ababababnt", "ababababnu", "ababababnv",
+         "ababababnw", "ababababnx", "ababababny", "ababababnz", "ababababoa", "ababababob", "ababababoc", "ababababod",
+         "ababababoe", "ababababof", "ababababog", "ababababoh", "ababababoi", "ababababoj", "ababababok", "ababababol",
+         "ababababom", "ababababon", "ababababoo", "ababababop", "ababababoq", "ababababor", "ababababos", "ababababot",
+         "ababababou", "ababababov", "ababababow", "ababababox", "ababababoy", "ababababoz", "ababababpa", "ababababpb",
+         "ababababpc", "ababababpd", "ababababpe", "ababababpf", "ababababpg", "ababababph", "ababababpi", "ababababpj",
+         "ababababpk", "ababababpl", "ababababpm", "ababababpn", "ababababpo", "ababababpp", "ababababpq", "ababababpr",
+         "ababababps", "ababababpt", "ababababpu", "ababababpv", "ababababpw", "ababababpx", "ababababpy", "ababababpz",
+         "ababababqa", "ababababqb", "ababababqc", "ababababqd", "ababababqe", "ababababqf", "ababababqg", "ababababqh",
+         "ababababqi", "ababababqj", "ababababqk", "ababababql", "ababababqm", "ababababqn", "ababababqo", "ababababqp",
+         "ababababqq", "ababababqr", "ababababqs", "ababababqt", "ababababqu", "ababababqv", "ababababqw", "ababababqx",
+         "ababababqy", "ababababqz", "ababababra", "ababababrb", "ababababrc", "ababababrd", "ababababre", "ababababrf",
+         "ababababrg", "ababababrh", "ababababri", "ababababrj", "ababababrk", "ababababrl", "ababababrm", "ababababrn",
+         "ababababro", "ababababrp", "ababababrq", "ababababrr", "ababababrs", "ababababrt", "ababababru", "ababababrv",
+         "ababababrw", "ababababrx", "ababababry", "ababababrz", "ababababsa", "ababababsb", "ababababsc", "ababababsd",
+         "ababababse", "ababababsf", "ababababsg", "ababababsh", "ababababsi", "ababababsj", "ababababsk", "ababababsl",
+         "ababababsm", "ababababsn", "ababababso", "ababababsp", "ababababsq", "ababababsr", "ababababss", "ababababst",
+         "ababababsu", "ababababsv", "ababababsw", "ababababsx", "ababababsy", "ababababsz", "ababababta", "ababababtb",
+         "ababababtc", "ababababtd", "ababababte", "ababababtf", "ababababtg", "ababababth", "ababababti", "ababababtj",
+         "ababababtk", "ababababtl", "ababababtm", "ababababtn", "ababababto", "ababababtp", "ababababtq", "ababababtr",
+         "ababababts", "ababababtt", "ababababtu", "ababababtv", "ababababtw", "ababababtx", "ababababty", "ababababtz",
+         "ababababua", "ababababub", "ababababuc", "ababababud", "ababababue", "ababababuf", "ababababug", "ababababuh",
+         "ababababui", "ababababuj", "ababababuk", "ababababul", "ababababum", "ababababun", "ababababuo", "ababababup",
+         "ababababuq", "ababababur", "ababababus", "ababababut", "ababababuu", "ababababuv", "ababababuw", "ababababux",
+         "ababababuy", "ababababuz", "ababababva", "ababababvb", "ababababvc", "ababababvd", "ababababve", "ababababvf",
+         "ababababvg", "ababababvh", "ababababvi", "ababababvj", "ababababvk", "ababababvl", "ababababvm", "ababababvn",
+         "ababababvo", "ababababvp", "ababababvq", "ababababvr", "ababababvs", "ababababvt", "ababababvu", "ababababvv",
+         "ababababvw", "ababababvx", "ababababvy", "ababababvz", "ababababwa", "ababababwb", "ababababwc", "ababababwd",
+         "ababababwe", "ababababwf", "ababababwg", "ababababwh", "ababababwi", "ababababwj", "ababababwk", "ababababwl",
+         "ababababwm", "ababababwn", "ababababwo", "ababababwp", "ababababwq", "ababababwr", "ababababws", "ababababwt",
+         "ababababwu", "ababababwv", "ababababww", "ababababwx", "ababababwy", "ababababwz", "ababababxa", "ababababxb",
+         "ababababxc", "ababababxd", "ababababxe", "ababababxf", "ababababxg", "ababababxh", "ababababxi", "ababababxj",
+         "ababababxk", "ababababxl", "ababababxm", "ababababxn", "ababababxo", "ababababxp", "ababababxq", "ababababxr",
+         "ababababxs", "ababababxt", "ababababxu", "ababababxv", "ababababxw", "ababababxx", "ababababxy", "ababababxz",
+         "ababababya", "ababababyb", "ababababyc", "ababababyd", "ababababye", "ababababyf", "ababababyg", "ababababyh",
+         "ababababyi", "ababababyj", "ababababyk", "ababababyl", "ababababym", "ababababyn", "ababababyo", "ababababyp",
+         "ababababyq", "ababababyr", "ababababys", "ababababyt", "ababababyu", "ababababyv", "ababababyw", "ababababyx",
+         "ababababyy", "ababababyz", "ababababza", "ababababzb", "ababababzc", "ababababzd", "ababababze", "ababababzf",
+         "ababababzg", "ababababzh", "ababababzi", "ababababzj", "ababababzk", "ababababzl", "ababababzm", "ababababzn",
+         "ababababzo", "ababababzp", "ababababzq", "ababababzr", "ababababzs", "ababababzt", "ababababzu", "ababababzv",
+         "ababababzw", "ababababzx", "ababababzy", "ababababzz"]))
     # print(Solution().findWords(board=[["o", "a", "a", "n"], ["e", "t", "a", "e"], ["i", "h", "k", "r"],
     #                                   ["i", "f", "l", "v"]], words=["oath", "pea", "eat", "rain"]))
