9108577fc19fbc3f43f019f568f9e1d3862dca26
wooilkim <wooilkim@korea.ac.kr>
2021-05-27 13:00:30 +0900

    solved PopularWords.py

---
+++ check.io/home/PopularWords.py
@@ -0,0 +1,31 @@
+# https://py.checkio.org/en/mission/popular-words/
+from collections import Counter
+
+
+def popular_words(text: str, words: list) -> dict:
+    counter = Counter(text.lower().split())
+    return dict((word, counter[word]) for word in words)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert popular_words('''
+When I was One
+I had just begun
+When I was Two
+I was nearly new
+''', ['i', 'was', 'three', 'near']) == {
+        'i': 4,
+        'was': 3,
+        'three': 0,
+        'near': 0
+    }
+    print("Coding complete? Click 'Check' to earn cool rewards!")
