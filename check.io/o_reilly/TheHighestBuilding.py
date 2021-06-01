3e53db15e460558fe0fe0580ba50247d1f5dd71b
wooilkim <wooilkim@korea.ac.kr>
2021-06-01 12:43:41 +0900

    solved TheHighestBuilding.py

--- check.io/o_reilly/TheHighestBuilding.py
+++ check.io/o_reilly/TheHighestBuilding.py
@@ -5,10 +5,8 @@ def highest_building(buildings):
     answer = [[i + 1, 0] for i in range(len(buildings[0]))]
     for j in range(len(buildings))[::-1]:
         for i in range(len(buildings[0])):
-            if answer[i][1] >= 0:
+            if buildings[j][i] == 1 and answer[i][1] == len(buildings) - (j + 1):
                 answer[i][1] += 1
-                if buildings[j][i] == 0:
-                    answer[i][1] *= -1
     return sorted(answer, key=lambda x: -abs(x[1]))[0]
 
 
