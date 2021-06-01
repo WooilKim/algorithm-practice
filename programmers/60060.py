e064f2667eef27194e31ecc5a5046a3f2ad88773
wooilkim <wooilkim@korea.ac.kr>
2021-06-01 17:16:46 +0900

    in progress

--- programmers/60060.py
+++ programmers/60060.py
@@ -2,9 +2,13 @@
 # 가사 검색
 # 2020 KAKAO BLIND RECRUITMENT
 
+from collections import defaultdict
+
+
 class Node:
     def __init__(self, key):
         self.key = key
+        self.len_dict = defaultdict(int)
         self.child = dict()
 
 
@@ -14,9 +18,10 @@ class Trie:
 
     def insert(self, word: str):
         cur = self.root
-        for w in word:
+        for i, w in enumerate(list(word)):
             if w not in cur.child:
                 cur.child[w] = Node(w)
+            cur.len_dict[len(word) - i] += 1
             cur = cur.child[w]
         cur.child['*'] = True
 
@@ -36,18 +41,27 @@ class Trie:
     #         if w == '?':
     #     return {}
 
-    def search_wildcard(self, prefix: str) -> bool:
-        l = len(prefix)
-        cnt = prefix.count('?')
+    def query(self, word: str) -> int:
         cur = self.root
-        for w in prefix:
+        for w in word:
+            if w == '?':
+                return sum(cur.len_dict.values())
             if w not in cur.child:
-                return False
+                return 0
             cur = cur.child[w]
-
-        return True
+        return 0
 
 
 def solution(words: list[int], queries: list[int]) -> list[int]:
     answer = []
+    trie = Trie()
+    for word in words:
+        trie.insert(word)
+
     return answer
+
+
+if __name__ == '__main__':
+    words = [["apple"], ["apple"], ["app"], ["app"], ["app"], ["app"]]
+
+    print(solution(words, words))
