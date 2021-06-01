1c13ca8f0d2e96241433a0618733120e224916bd
wooilkim <wooilkim@korea.ac.kr>
2021-06-01 12:43:33 +0900

    solve FlattenList.py

---
+++ check.io/o_reilly/FlattenList.py
@@ -0,0 +1,15 @@
+# https://py.checkio.org/en/mission/flatten-list/
+# Flatten A List
+
+def flat_list(array):
+    flatten = lambda elements: (num for elem in elements for num in
+                                (flatten(elem) if isinstance(elem, list) else [elem]))
+    return list(flatten(array))
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
