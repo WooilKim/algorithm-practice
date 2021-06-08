d1f6c341a1e953b29e8b27223a8711619298dbf7
wooilkim <wooilkim@korea.ac.kr>
2021-06-08 16:01:43 +0900

    solved AcceptablePasswordIV.py

---
+++ check.io/electronic_station/AcceptablePasswordIV.py
@@ -0,0 +1,25 @@
+# https://py.checkio.org/en/mission/acceptable-password-iv/
+# Electronic station
+# Acceptable Password IV
+
+# Taken from mission Acceptable Password III
+
+def is_acceptable_password(password: str) -> bool:
+    return (len(password) > 6 and any([c.isnumeric() for c in password]) and any([c.isalpha() for c in password])) or \
+           (len(password) > 9)
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('short54') == True
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    assert is_acceptable_password('1234567') == False
+    assert is_acceptable_password('12345678910') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
