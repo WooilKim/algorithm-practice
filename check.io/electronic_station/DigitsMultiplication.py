57d59599784b2e2e1b46cd2d9f9c9eb81cf8b8c7
wooilkim <wooilkim@korea.ac.kr>
2021-06-08 10:26:14 +0900

    solved DigitsMultiplication.py

---
+++ check.io/electronic_station/DigitsMultiplication.py
@@ -0,0 +1,18 @@
+# https://py.checkio.org/en/mission/digits-multiplication/
+from functools import reduce
+
+
+def checkio(number: int) -> int:
+    return reduce(lambda x, y: x * y if y != 0 else x, map(int, str(number)))
+
+
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio(123405))
+
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
