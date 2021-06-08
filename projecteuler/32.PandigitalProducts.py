5827aa77186ba3426bdfaba9a0d999e787a74f44
wooilkim <wooilkim@korea.ac.kr>
2021-06-08 11:28:52 +0900

    arranged 32.PandigitalProducts.py

--- projecteuler/32.PandigitalProducts.py
+++ projecteuler/32.PandigitalProducts.py
@@ -2,15 +2,10 @@
 # Problem 32. Pandigital Products
 
 def pandigital_products():
-    def is_pandigital(a, b, c):
-        return len(f'{a}{b}{c}') == 9 and all([True if a == b else False for a, b in
-                                               zip(sorted(list(map(int, f'{a}{b}{c}'))), [i for i in range(1, 10)])])
-
-    print([i for i in range(1, 10)])
     res = set()
-    for i in range(1, 10000):
+    for i in range(1, 100):
         for j in range(1, 10000):
-            if is_pandigital(i, j, i * j):
+            if ''.join(sorted(f'{i}{j}{i * j}')) == '123456789':
                 res.add(i * j)
     return res
 
