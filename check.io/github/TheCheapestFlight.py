1c493599946de01e2e38e55c985b76d26d925925
wooilkim <wooilkim@korea.ac.kr>
2021-06-18 11:02:57 +0900

    solved TheCheapestFlight.py

---
+++ check.io/github/TheCheapestFlight.py
@@ -0,0 +1,24 @@
+# https://py.checkio.org/en/mission/the-cheapest-flight/
+# Github
+# The Cheapest Flight
+from typing import List
+from collections import defaultdict
+
+
+def cheapest_flight(costs: List, a: str, b: str) -> int:
+    graph = defaultdict(dict)
+    for start, target, price in costs:
+        graph[start][target] = price
+        graph[target][start] = price
+    res = list()
+
+    def dfs(path, p):
+        if path[-1] == b:
+            res.append(p)
+        else:
+            for key in graph[path[-1]].keys():
+                if key not in path:
+                    dfs(path + key, p + graph[path[-1]][key])
+
+    dfs(a, 0)
+    return min(res) if res else 0
